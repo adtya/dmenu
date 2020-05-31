eaed0d0b881058e82dac5389219107bde1f6c3be
Adithya Nair <mail@adtya.xyz>
2020-05-31 21:38:39 +0530

    fix crash from trying to add color emojis

--- config.def.h
+++ config.def.h
@@ -4,14 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Fira Code:size=12",
+	/*"Noto Color Emoji:pixelsize=10" */
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#f8f8f2", "#222222" },
+	[SchemeSel] = { "#292a36", "#8be9fd" },
+	[SchemeOut] = { "#f8f8f2", "#282a36" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
